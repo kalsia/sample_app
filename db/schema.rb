cf3441e4d6f5d5b9ac692dfa776e09328bd045a2
marco <marco.gaiazzi.ggl@gmail.com>
2014-01-15 18:33:25 +0100

    Finish user CRUD actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140113143900) do
+ActiveRecord::Schema.define(:version => 20140115171620) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,16 @@ describe User do
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
